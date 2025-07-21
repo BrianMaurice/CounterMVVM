5a68ca24a4b9d16578ea500258b135ed3b7fa5fa
Brian Maurice <briankinyua99@gmail.com>
2025-07-21 10:47:32 +0300

    More on mvvm with compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,11 @@ android {
 }
 
 dependencies {
+    val lifecycle_version = "2.9.2"
 
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/countermvvm/MainActivity.kt
+++ app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.countermvvm.ui.theme.CounterMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +33,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel: CounterViewModel = viewModel()
             CounterMVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    Greeting(viewModel)
                 }
             }
         }
@@ -31,18 +44,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TheCounterApp(viewModel: CounterViewModel){
+    val count = remember{mutableStateOf(0)}
+
+
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+
+        Text(text = "Count: ${viewModel.count.value}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Button(onClick = {viewModel.increment()}) {
+                Text("Increment")
+            }
+            Button(onClick = {viewModel.decrement()}) {
+                Text("Decrement")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(viewModel: CounterViewModel) {
+    TheCounterApp(viewModel)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CounterMVVMTheme {
-        Greeting("Android")
-        Greeting("Android")
+        Greeting(viewModel())
     }
 }
